235298ba4ef7428636c2d3b98e8bb913f2f84d35
Forstie <Ryan.Forstie1@T-Mobile.com>
2020-02-10 15:10:23 -0800

    Add logic to check when there is a match

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { lorem } from "faker";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = "";
+
+  onInput(value: string) {
+    this.enteredText = value;
+  }
 }
